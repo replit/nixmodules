310c889983531a32ce9c19327cf7b134f5a5b7bc
Toby Ho <toby@repl.it>
2023-05-04 18:06:46 -0400

    updated prybar url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix expressions for defining Replit development environments";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=52e3e80afff4b16ccb7c52e9f0f5220552f03d04";
   inputs.nixmodules-stable.url = "github:replit/nixmodules?rev=d77c07009d6d0e09eaf7aa011cad530a644eca01";
-  inputs.prybar.url = "/home/toby/replit/prybar";
+  inputs.prybar.url = "github:replit/prybar?rev=65f486534054665f1b333689417c39acd370d3a5";
 
   outputs = { self, nixpkgs, nixmodules-stable, prybar, ... }:
     let
