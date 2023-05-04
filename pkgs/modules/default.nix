d591575d64b999baf22368387538a5c977215e4a
Toby Ho <toby@repl.it>
2023-05-04 18:02:10 -0400

    added all modules

--- pkgs/modules/default.nix
+++ pkgs/modules/default.nix
@@ -5,9 +5,39 @@ let
   };
   
   modulesList = [
+    (mkModule (import ./python {
+      python = pkgs.python310Full;
+      pypkgs = pkgs.python310Packages;
+    }))
+    (mkModule (import ./nodejs {
+      nodejs = pkgs.nodejs-14_x;
+    }))
+    (mkModule (import ./nodejs {
+      nodejs = pkgs.nodejs-19_x;
+    }))
+    (mkModule (import ./nodejs {
+      nodejs = pkgs.nodejs-18_x;
+    }))
+    (mkModule (import ./nodejs {
+      nodejs = pkgs.nodejs-16_x;
+    }))
     (mkModule ./go)
     (mkModule ./rust)
     (mkModule ./swift)
+    (mkModule ./bun)
+    (mkModule ./c)
+    (mkModule ./cpp)
+    (mkModule ./dart)
+    (mkModule ./clojure)
+    (mkModule ./dotnet)
+    (mkModule ./haskell)
+    (mkModule ./java)
+    (mkModule ./lua)
+    (mkModule ./php)
+    (mkModule ./qbasic)
+    (mkModule ./R)
+    (mkModule ./ruby)
+    (mkModule ./web)
   ];
 
   modules = builtins.listToAttrs (
