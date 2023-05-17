3a9d3227c7c483a89c434c64591c05bf137c5dfc
Toby Ho <toby@repl.it>
2023-05-17 16:23:28 -0400

    added upgrade mappings

--- pkgs/upgrade-maps/default.nix
+++ pkgs/upgrade-maps/default.nix
@@ -2,8 +2,12 @@
 let
 mapping = {
   # Examples:
-  # "nodejs-18-v1.1" = { to = "nodejs-18-v1.2"; auto = true; };
-  # "nodejs-18-v1.2" = { to = "bun-0.5-v1.1"; };
+  "nodejs-14-v1.1" = { to = "nodejs-14-v1.2"; auto = true; };
+  "nodejs-16-v1.1" = { to = "nodejs-16-v1.2"; auto = true; };
+  "nodejs-18-v1.1" = { to = "nodejs-18-v1.2"; auto = true; };
+  "nodejs-19-v1.1" = { to = "nodejs-19-v1.2"; auto = true; };
+  "ruby-3.1-v1.0" = { to = "ruby-3.1-v1.1"; auto = true; };
+  "nodejs-18-v1.2" = { to = "bun-0.5-v1.0"; };
 };
 in with pkgs.lib.attrsets; {
   auto = mapAttrs (mod: entry: entry.to) (filterAttrs (mod: entry: entry.auto or false) mapping);
