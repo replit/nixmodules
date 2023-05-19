a0dfb78744e48a02be766ff19699837ae847ae0f
Toby Ho <toby@repl.it>
2023-05-19 15:14:08 -0400

    change to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Nix expressions for defining Replit development environments";
   inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=52e3e80afff4b16ccb7c52e9f0f5220552f03d04";
-  inputs.nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+  inputs.nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
   inputs.prybar.url = "github:replit/prybar?rev=65f486534054665f1b333689417c39acd370d3a5";
 
   outputs = { self, nixpkgs, nixpkgs-unstable, prybar, ... }:
