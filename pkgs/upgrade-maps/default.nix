ca58873096a6babc584bab542c5c38ec06c3c2a8
Toby Ho <toby@repl.it>
2023-05-24 17:50:10 -0400

    tweak

--- pkgs/upgrade-maps/default.nix
+++ pkgs/upgrade-maps/default.nix
@@ -15,7 +15,8 @@ mapping = {
 
 present-entries = entries: mapAttrs (mod: entry: 
 (
-  if builtins.hasAttr "changelog" entry then ({ inherit (entry) to changelog; })
+  if builtins.hasAttr "changelog" entry 
+  then ({ inherit (entry) to changelog; })
   else ({ inherit (entry) to; })
 )) entries;
 filter-auto = filterAttrs (mod: entry: entry.auto or false);
