d608d2d86ec5746ad3f7e2cedee6d2646ba37b4d
Toby Ho <toby@repl.it>
2023-05-30 14:42:54 -0400

    added moduleit example to ci check

--- scripts/ci_check.sh
+++ scripts/ci_check.sh
@@ -2,20 +2,30 @@
 set -exuo pipefail
 
 NIX_FLAGS="--extra-experimental-features nix-command --extra-experimental-features flakes --extra-experimental-features discard-references"
+
 echo "Evaluate modules derivations"
 nix eval $NIX_FLAGS .#modules --json
+
 echo "Build upgrade maps"
 nix build $NIX_FLAGS .#upgrade-maps
 cat result/auto-upgrade.json
 cat result/recommend-upgrade.json
+
 echo "Build active modules"
 nix build $NIX_FLAGS .#active-modules
 cat result
 nix develop $NIX_FLAGS
+
 echo "Verify modules.json"
 python scripts/lock_modules.py -v
 python scripts/check_modules.py
+
 echo "Verify upgrade maps"
 python scripts/check_upgrade_maps.py
+
 echo "Build new/updated modules"
 python scripts/build_changed_modules.py origin/main
+
+echo "Build moduleit example"
+cd pkgs/moduleit
+./moduleit.sh example.nix
