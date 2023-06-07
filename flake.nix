0e08414df952c36396e4031ced11317626a932ec
Colton Donnelly <colton@donn.io>
2023-06-07 20:49:17 +0000

    set nixpkgs to the latest 23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Nix expressions for defining Replit development environments";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=52e3e80afff4b16ccb7c52e9f0f5220552f03d04";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs?rev=87f9156865ab09e3bde39aadb4131ae364ae704e";
   inputs.prybar.url = "github:replit/prybar?rev=65f486534054665f1b333689417c39acd370d3a5";
 
