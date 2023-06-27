55e403e578f777fe6cffd81d455f0d03072f7216
Colton Donnelly <colton@donn.io>
2023-06-27 13:54:12 +0000

    it's close

--- pkgs/modules/rust/default.nix
+++ pkgs/modules/rust/default.nix
@@ -1,20 +1,22 @@
 { pkgs, lib, ... }:
 let
+  toolchain = pkgs.fenix.stable;
+
   cargoRun = pkgs.writeScriptBin "cargo_run" ''
     if [ ! -f "$HOME/$REPL_SLUG/Cargo.toml" ]; then
       NAME=$(echo $REPL_SLUG | sed -r 's/([a-z0-9])([A-Z])/\1_\2/g'| tr '[:upper:]' '[:lower:]')
-      ${pkgs.cargo}/bin/cargo init --name=$NAME
+      ${toolchain.cargo}/bin/cargo init --name=$NAME
     fi
 
-    ${pkgs.cargo}/bin/cargo run
+    ${toolchain.cargo}/bin/cargo run
   '';
-  rust-version = lib.versions.majorMinor pkgs.rustc.version;
+  rust-version = lib.versions.majorMinor toolchain.rustc.version;
 in
 {
   id = "rust-${rust-version}";
   name = "Rust Tools";
 
-  packages = with pkgs; [
+  packages = with toolchain; [
     cargo
     clang
     rustc
@@ -34,14 +36,14 @@ in
     name = "rust-analyzer";
     language = "rust";
 
-    start = "${pkgs.rust-analyzer}/bin/rust-analyzer";
+    start = "${toolchain.rust-analyzer}/bin/rust-analyzer";
   };
 
   replit.formatters.cargo-fmt = {
     name = "cargo fmt";
     language = "rust";
 
-    start = "${pkgs.cargo}/bin/cargo fmt";
+    start = "${toolchain.cargo}/bin/cargo fmt";
     stdin = false;
   };
 
@@ -49,7 +51,7 @@ in
     name = "rustfmt";
     language = "rust";
 
-    start = "${pkgs.rustfmt}/bin/rustfmt $file";
+    start = "${toolchain.rustfmt}/bin/rustfmt $file";
     stdin = false;
   };
 
