eb29cca1bc856c48a88bdd41ef7b4a18286f58c4
Toby Ho <toby@repl.it>
2023-07-11 11:17:13 -0400

    use pyright-extended in python module

--- pkgs/modules/pyright-extended/default.nix
+++ pkgs/modules/pyright-extended/default.nix
@@ -1,48 +1,11 @@
 { pkgs, lib, ... }:
 let
-  version = "1.1.317";
-
-  pyright-extended = pkgs.stdenvNoCC.mkDerivation rec {
-    pname = "pyright-extended";
-    inherit version;
-
-    src = pkgs.fetchurl {
-      url = "https://registry.npmjs.org/@replit/pyright-extended/-/pyright-extended-${version}.tgz";
-      hash = "sha256-I/NAp7EjX0a4jzXWczUyOPC8ZFYZibPQLH5vHb9ixZU=";
-    };
-
-    binPath = lib.makeBinPath [
-      pkgs.ruff
-      pkgs.yapf
-      pkgs.nodejs_18
-    ];
-
-    nativeBuildInputs = [
-      pkgs.makeWrapper
-    ];
-
-    dontConfigure = true;
-    dontBuild = true;
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/lib
-      cp -r . $out/lib
-      mkdir -p $out/bin
-      makeWrapper "$out/lib/index.js" "$out/bin/index.js" \
-        --prefix PATH : "${binPath}"
-      makeWrapper "$out/lib/langserver.index.js" "$out/bin/langserver.index.js" \
-        --prefix PATH : "${binPath}"
-      runHook postInstall
-    '';
-  };
-
+  pyright-extended = pkgs.callPackage ../../pyright-extended { };
 in
 {
   id = "pyright-extended";
   name = "pyright-extended LSP";
-
-  replit.languageServers.python-lsp-server = {
+  replit.languageServers.pyright-extended = {
     name = "pyright-extended";
     language = "python3";
     start = "${pyright-extended}/bin/langserver.index.js --stdio";
