f8f740e9132150cec83f1a86056fbda433d5752d
Toby Ho <toby@repl.it>
2023-08-17 15:02:15 +0000

    try

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -90,7 +90,7 @@ rec {
     # customize these IDs for dev. They can be like "python-3.10:v10-20230711-6807d41" or "python-3.10"
     # publish your feature branch first and make sure modules.json is current, then
     # in goval dir (next to nixmodules), run `make custom-nixmodules-disk` to use this disk in conman
-    moduleIds = [ "python-3.10" "nodejs-18" ];
+    moduleIds = [ "python-3.10:v5-20230613-622effa" ];
     inherit upgrade-maps;
   };
 
