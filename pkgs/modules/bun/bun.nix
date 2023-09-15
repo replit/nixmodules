80b0f23e908a40de968957b1456327138e3e94e3
Colton Donnelly <colton@donn.io>
2023-09-15 20:27:30 +0000

    upgrade bun to 1.0.2

--- pkgs/modules/bun/bun.nix
+++ pkgs/modules/bun/bun.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.0.1";
+  version = "1.0.2";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-RmgnWTG6kTebYwIa/VAwvvJmbL+ARNC+HkbF4mJPF7o=";
+    hash = "sha256-kHv8PU48Le4lG3pf304hXggAtx/I5uBeu4aHmLsbdgw=";
   };
 }
