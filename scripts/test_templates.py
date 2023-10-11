1d0c183615347b4182b18643e9b68633cbd86b0c
Toby Ho <toby@repl.it>
2023-10-11 12:55:04 -0400

    new version mismatch scheme and use secret from kube

--- scripts/test_templates.py
+++ scripts/test_templates.py
@@ -6,32 +6,32 @@ import time
 from time import sleep
 import sys
 import argparse
+import subprocess
+import base64
 
-jitter_wait = 5 * 60
-jitter_timeout = 3600
+global_timeout = 3600
+wait_when_version_mismatch = 15 * 60
+test_poll_wait = 10
 
-TEMPLATE_TESTER_URL = "https://templatetester.tobyho.repl.co"
-HEADERS = {
-  "Authorization": "BLARGH",
-}
+TEMPLATE_TESTER_URL = "https://templatetester-1.tobyho.repl.co"
 TEMPLATES_TO_TEST = [
   "@replit/Blank-Repl",
   "@replit/Python",
   "@replit/Nodejs",
   "@replit/Nodejs-Beta",
   # "@replit/Java",
-#   "@replit/CSharp",
-#   "@replit/HTML-CSS-JS",
-#   "@replit/Go",
-#   "@replit/C",
-#   "@replit/Python-with-Turtle",
-#   "@replit/Pygame-Beta",
-#   "@replit/TypeScript",
-#   "@replit/React-Javascript",
-#   "@replit/PHP-Web-Server",
-#   "@replit/Kaboom",
-#   "@replit/Rust",
-#   "@replit/Bun",
+  "@replit/CSharp",
+  "@replit/HTML-CSS-JS",
+  "@replit/Go",
+  "@replit/C",
+  "@replit/Python-with-Turtle",
+  "@replit/Pygame-Beta",
+  "@replit/TypeScript",
+  "@replit/React-Javascript",
+  "@replit/PHP-Web-Server",
+  "@replit/Kaboom",
+  "@replit/Rust",
+  "@replit/Bun",
 ]
 
 def main():
@@ -42,91 +42,51 @@ def main():
   parser.add_argument('-p', '--pid1-version')
   args = parser.parse_args()
 
-  if args.nix_modules_version is not None and args.pid1_version is not None:
-    raise Exception("Please only provide one of -m and -p")
+  auth = get_jobs_auth()
 
-  if args.nix_modules_version:
-    wait_till_nix_modules_matches(args.nix_modules_version)
-  elif args.pid1_version:
-    wait_till_pid1_matches(args.pid1_version)
-
-  job_id = create_test_job()
-  print("Started job %d" % job_id)
-  test_poll_wait = 10
-  while True:
-    sleep(test_poll_wait)
-    reply = get_test_job_status(job_id)
-    if reply["complete"]:
-      passed = print_test_results(reply["TestRuns"])
-      if passed:
-        print("Pass")
-      else:
-        print("Fail")
-        exit(1)
-      break
-    else:
-      print(".", end="")
-      sys.stdout.flush()
-
-def wait_till_nix_modules_matches(expected_version):
   start = time.time()
   while True:
-    print("checking Nix modules version...")
-    version = get_nix_modules_version()
-    if version != expected_version:
-      print("Nix modules version %s does not match expected: %s" % (version, expected_version))
-      if time.time() - start > jitter_timeout:
-        raise Exception("Giving up!")
-      sleep(jitter_wait)
-    else:
-      print("Nix modules version matches!")
-      break
+    job_id = create_test_job(args, auth)
+    print("Started job %d" % job_id)
+    while True:
+      reply = get_test_job_status(job_id, auth)
+      if reply["complete"]:
+        test_runs = reply["TestRuns"]
+        if version_mismatch(test_runs):
+          duration = time.time() - start
+          if duration > global_timeout:
+            print("Giving up after %ds" % duration)
+            exit(1)
+        print(".", end="")
+        sys.stdout.flush()
+        sleep(test_poll_wait)
 
-def wait_till_pid1_matches(expected_version):
-  start = time.time()
-  while True:
-    print("checking Pid1 modules version...")
-    version = get_pid1_version()
-    if version != expected_version:
-      print("Pid1 version %s does not match expected: %s" % (version, expected_version))
-      if time.time() - start > jitter_timeout:
-        raise Exception("Giving up!")
-      sleep(jitter_wait)
-    else:
-      print("Pid1 version matches!")
-      break
-
-def get_nix_modules_version():
-  resp = requests.get(
-    "%s/nix-modules-version" % TEMPLATE_TESTER_URL,
-    headers = HEADERS
-  )
-  return resp.json()["version"]
-
-def get_pid1_version():
-  resp = requests.get(
-    "%s/pid1-version" % TEMPLATE_TESTER_URL,
-    headers = HEADERS
-  )
-  return resp.json()["version"]
-
-def create_test_job():
+def create_test_job(args, auth):
   resp = requests.post(
     "%s/jobs" % TEMPLATE_TESTER_URL,
     json = {
-      "templates": TEMPLATES_TO_TEST
+      "templates": TEMPLATES_TO_TEST,
+      "nixModulesVersion": args.nix_modules_version,
+      "pid1Version": args.pid1_version,
     },
-    headers = HEADERS
+    headers = { 'Authorization': auth }
   )
   reply = resp.json()
   return reply["jobId"]
 
-def get_test_job_status(job_id):
+def get_test_job_status(job_id, auth):
   resp = requests.get(
     "%s/jobs/%d" % (TEMPLATE_TESTER_URL, job_id),
-    headers = HEADERS
+    headers = { 'Authorization': auth }
   )
   return resp.json()
+
+def version_mismatch(test_runs):
+  for run in test_runs:
+    result = run["result"]
+    if result == "MV" or result == "PV":
+      return True
+  return False
 
 def print_test_results(test_runs):
   print()
@@ -138,6 +98,15 @@ def print_test_results(test_runs):
     if result != "P" and result != "S":
       passed = False
   return passed
+
+def get_jobs_auth():
+  output = subprocess.check_output([
+    'kubectl', 'get',
+    'secret', 'jobs-auth', 
+    '-n', 'nixmodules', '-o',
+    'jsonpath={.data.jobs-auth}'
+  ])
+  return str(base64.b64decode(output), 'UTF-8')
 
 if __name__ == "__main__":
   main()
