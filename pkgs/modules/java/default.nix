4260f4184fb511e6cd2d07173853e4c9180edee4
Colton Donnelly <colton@donn.io>
2023-11-27 23:17:10 +0000

    disable java module

--- pkgs/modules/java/default.nix
+++ pkgs/modules/java/default.nix
@@ -1,9 +1,11 @@
+# TODO: fix this
+# there's maven issues with java-debug and java-language-server
 { pkgs, lib, ... }:
 
 let
   graalvm = pkgs.graalvm-ce;
 
-  graalvm-version = lib.versions.majorMinor graalvm.version;
+  graalvm-version = lib.versions.major graalvm.version;
 
   graal-compile-command = "${graalvm}/bin/javac -classpath .:target/dependency/* -d . $(find . -type f -name '*.java')";
 
@@ -13,10 +15,13 @@ let
     jdk = pkgs.graalvm-ce;
   };
 
-  java-debug = pkgs.callPackage ../../java-debug {
-    inherit jdt-language-server;
-    jdk = pkgs.graalvm-ce;
-  };
+  # TODO: java-debug doesn't work with java 21 :( gotta either update the repo or
+  # use a different graalvm version, which i'm not 100% convinced will work off
+  # the bat but also this nixmodule is currently unused so just kicking cans i guess
+  # java-debug = pkgs.callPackage ../../java-debug {
+  #   inherit jdt-language-server;
+  #   jdk = pkgs.graalvm-ce;
+  # };
 
   run-lsp = pkgs.writeShellScriptBin "run-lsp" ''
     # Allow setting this env var to diagnose the lsp
@@ -30,7 +35,7 @@ let
 in
 
 {
-  id = "java-graalvm${graalvm-version}";
+  id = "java-${graalvm-version}-graalvm";
   name = "Java Tools (with Graal VM)";
 
   replit.packages = [
@@ -56,41 +61,41 @@ in
     };
   };
 
-  replit.dev.debuggers.java-debug = {
-    name = "Jave Debug";
-    language = "java";
-    extensions = [ ".java" ];
+  # replit.dev.debuggers.java-debug = {
+  # name = "Jave Debug";
+  # language = "java";
+  # extensions = [ ".java" ];
 
-    transport = "localhost:0";
-    compile = graal-compile-command;
-    start = "${java-debug}/bin/java-debug";
+  # transport = "localhost:0";
+  # compile = graal-compile-command;
+  # start = "${java-debug}/bin/java-debug";
 
-    initializeMessage = {
-      command = "initialize";
-      arguments = {
-        adapterID = "cppdbg";
-        clientID = "replit";
-        clientName = "replit.com";
-        columnsStartAt1 = true;
-        linesStartAt1 = true;
-        locale = "en-us";
-        pathFormat = "path";
-        supportsInvalidatedEvent = true;
-        supportsProgressReporting = true;
-        supportsRunInTerminalRequest = true;
-        supportsVariablePaging = true;
-        supportsVariableType = true;
-      };
-    };
+  # initializeMessage = {
+  # command = "initialize";
+  # arguments = {
+  # adapterID = "cppdbg";
+  # clientID = "replit";
+  # clientName = "replit.com";
+  # columnsStartAt1 = true;
+  # linesStartAt1 = true;
+  # locale = "en-us";
+  # pathFormat = "path";
+  # supportsInvalidatedEvent = true;
+  # supportsProgressReporting = true;
+  # supportsRunInTerminalRequest = true;
+  # supportsVariablePaging = true;
+  # supportsVariableType = true;
+  # };
+  # };
 
-    launchMessage = {
-      command = "launch";
-      arguments = {
-        classPaths = [ "." ];
-        mainClass = "Main";
-      };
-    };
-  };
+  # launchMessage = {
+  # command = "launch";
+  # arguments = {
+  # classPaths = [ "." ];
+  # mainClass = "Main";
+  # };
+  # };
+  # };
 
   replit.dev.languageServers.java-language-server = {
     name = "Java Language Server";
