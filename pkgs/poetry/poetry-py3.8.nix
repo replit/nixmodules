fdd0b28c5f8599af9b7e8cdc75a84ec439065952
Toby Ho <toby@repl.it>
2023-12-11 14:41:04 -0500

    upgrade poetry; use no parallel workers for free accounts

--- pkgs/poetry/poetry-py3.8.nix
+++ pkgs/poetry/poetry-py3.8.nix
@@ -1,7 +1,7 @@
 { pkgs, python, pypkgs }:
 pkgs.callPackage ./poetry-in-venv.nix {
-  version = "1.5.1";
-  url = "https://storage.googleapis.com/poetry-bundles/poetry-1.5.1-python-3.8.15-bundle.tgz";
-  sha256 = "sha256:0j25p10msnzxm75gc4q132ndj9j2igwqhkiy4bmidxp2dlrzxqmc";
+  version = "1.5.2";
+  url = "https://storage.googleapis.com/poetry-bundles/poetry-1.5.2-python-3.8.17-bundle.tgz";
+  sha256 = "sha256:14bgmas1qnkk0ndisphn2ciidffi7mpmd3hibzbs1as8hpwpkw5m";
   inherit python pypkgs;
 }
