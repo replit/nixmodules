fdbad0c30bfee3ed47d31e02f95e4940ecdfd149
Toby Ho <toby@repl.it>
2023-12-12 14:35:03 -0500

    Revert "Poetry perf enhancement"

--- pkgs/poetry/poetry-py3.10.nix
+++ pkgs/poetry/poetry-py3.10.nix
@@ -1,7 +1,7 @@
 { pkgs, python, pypkgs }:
 pkgs.callPackage ./poetry-in-venv.nix {
-  version = "1.5.2";
-  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.2-python-3.10.11-bundle.tgz;
-  sha256 = "sha256:0qabdvan0f2fidy9czdlhbl7j3wsnsqkjnwblq52ljlbzrgnp9ba";
+  version = "1.5.1";
+  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.1-bundle.tgz;
+  sha256 = "sha256:1qh1w1dr2wvqla4cdxcgvl9xipcyk31mapivcp66v92mkvpayygk";
   inherit python pypkgs;
 }
