0c5d1c6d5a3c687b88bb8ecff8cedad7d99ffcd5
Replit user <>
2023-12-15 23:02:41 +0000

    Re-land. Updated poetry bundles.

--- pkgs/poetry/poetry-py3.10.nix
+++ pkgs/poetry/poetry-py3.10.nix
@@ -1,7 +1,7 @@
 { pkgs, python, pypkgs }:
 pkgs.callPackage ./poetry-in-venv.nix {
-  version = "1.5.1";
-  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.1-bundle.tgz;
-  sha256 = "sha256:1qh1w1dr2wvqla4cdxcgvl9xipcyk31mapivcp66v92mkvpayygk";
+  version = "1.5.3";
+  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.3-python-3.10.11-bundle.tgz;
+  sha256 = "sha256:10jh59xcj0yx9hh97w47ixr1np7szw9y62zxqy9phzpk6yc2qm68";
   inherit python pypkgs;
 }
