04ec6d9b18fcda08e050294937403f1507354853
Devon Stewart <devon.stewart@repl.it>
2024-01-05 14:26:04 -0800

    Consistency

--- pkgs/poetry/poetry-py3.8.nix
+++ pkgs/poetry/poetry-py3.8.nix
@@ -1,7 +1,7 @@
 { pkgs, python, pypkgs }:
 pkgs.callPackage ./poetry-in-venv.nix {
   version = "1.5.3";
-  url = "https://storage.googleapis.com/poetry-bundles/poetry-1.5.3-python-3.8.17-bundle.tgz";
+  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.3-python-3.8.17-bundle.tgz;
   sha256 = "sha256:0vazh05z7zfwbgbykq6xaba6dkfhvspf9rikpvqnnic5v7izm502";
   inherit python pypkgs;
 }
