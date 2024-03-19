06ff798f02e161f624e7aa8e4d5482c109974c3c
Toby Ho <toby@repl.it>
2024-03-18 20:54:50 -0400

    implementation for Node.js modules

---
+++ pkgs/modules/packagers/nodejs-packager/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, lib, config, ... }:
+let
+  cfg = config.packagers.nodejs-packager;
+  nodejs = pkgs.${"nodejs_${config.interpreters.nodejs.version}"};
+in
+with pkgs.lib; {
+  options = {
+    packagers.nodejs-packager.enable = mkModuleEnableOption {
+      name = "Node.js Packager";
+      description = "Package management for Node.js: supports one of npm, yarn, and pnpm";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    replit.dev.packagers.nodejs-packager = {
+      name = "Node.js packager";
+      language = "nodejs";
+      displayVersion = "Node ${nodejs.version}";
+      features = {
+        packageSearch = true;
+        guessImports = true;
+        enabledForHosting = false;
+      };
+    };
+  };
+}
