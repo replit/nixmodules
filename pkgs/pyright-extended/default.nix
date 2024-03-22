590b495cb4e99b838af40b3a86956429aea61d56
Devon Stewart <devon.stewart@repl.it>
2024-03-22 18:47:16 +0000

    Bump pyright

--- pkgs/pyright-extended/default.nix
+++ pkgs/pyright-extended/default.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, yapf, ... }:
 let
-  version = "2.0.9";
+  version = "2.0.10";
 in
 pkgs.stdenvNoCC.mkDerivation rec {
   pname = "pyright-extended";
@@ -8,7 +8,7 @@ pkgs.stdenvNoCC.mkDerivation rec {
 
   src = pkgs.fetchurl {
     url = "https://registry.npmjs.org/@replit/pyright-extended/-/pyright-extended-${version}.tgz";
-    hash = "sha256-njW78ZaVe1CupkyfT0LZQD/6XwROu6w6nhJSUFKC3MA";
+    hash = "sha256-fDxuX1Yyo26m1RB+LQbMBrsqljLU/iOYy26scMTwinw=";
   };
 
   binPath = lib.makeBinPath [
