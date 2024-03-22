d09f124c835f1b24d75cc76fb76b7cb20b21c2a5
Toby Ho <toby@repl.it>
2024-03-22 20:08:14 +0000

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,30 +1,30 @@
 {
   description = "Nix expressions for defining Replit development environments";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/?rev=7c4c20509c4363195841faa6c911777a134acdf3";
-  inputs.fenix.url = "github:nix-community/fenix";
-  inputs.fenix.inputs.nixpkgs.follows = "nixpkgs-unstable";
-  inputs.nixd.url = "github:nix-community/nixd";
-  inputs.nixd.inputs.nixpkgs.follows = "nixpkgs-unstable";
-  inputs.prybar.url = "github:replit/prybar";
-  inputs.prybar.inputs.nixpkgs.follows = "nixpkgs-unstable";
-  inputs.java-language-server.url = "github:replit/java-language-server";
-  inputs.java-language-server.inputs.nixpkgs.follows = "nixpkgs-unstable";
+  # inputs.fenix.url = "github:nix-community/fenix";
+  # inputs.fenix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+  # inputs.nixd.url = "github:nix-community/nixd";
+  # inputs.nixd.inputs.nixpkgs.follows = "nixpkgs-unstable";
+  # inputs.prybar.url = "github:replit/prybar";
+  # inputs.prybar.inputs.nixpkgs.follows = "nixpkgs-unstable";
+  # inputs.java-language-server.url = "github:replit/java-language-server";
+  # inputs.java-language-server.inputs.nixpkgs.follows = "nixpkgs-unstable";
   # inputs.ztoc-rs.url = "github:replit/ztoc-rs";
   # inputs.ztoc-rs.inputs.nixpkgs.follows = "nixpkgs-unstable";
   inputs.replit-rtld-loader.url = "github:replit/replit_rtld_loader";
   inputs.replit-rtld-loader.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, prybar, java-language-server, nixd, fenix, replit-rtld-loader, ... }:
+  outputs = { self, nixpkgs-unstable, replit-rtld-loader, ... }:
     let
       nixpkgs = nixpkgs-unstable;
       mkPkgs = nixpkgs-spec: system: import nixpkgs-spec {
         inherit system;
         overlays = [
           self.overlays.default
-          prybar.overlays.default
-          java-language-server.overlays.default
-          nixd.overlays.default
-          fenix.overlays.default
+          # prybar.overlays.default
+          # java-language-server.overlays.default
+          # nixd.overlays.default
+          # fenix.overlays.default
           replit-rtld-loader.overlays.default
         ];
         # replbox has an unfree license
