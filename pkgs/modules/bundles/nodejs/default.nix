549eacc81330fea45ea26a617961dfeae2349f06
Toby Ho <toby@repl.it>
2024-04-04 17:40:09 -0400

    implemented mkBundleModule automating making bundles and exposing bundle submodules

--- pkgs/modules/bundles/nodejs/default.nix
+++ pkgs/modules/bundles/nodejs/default.nix
@@ -1,20 +1,14 @@
-{ pkgs, lib, config, ... }:
-let
-  cfg = config.bundles.nodejs;
-in
-with pkgs.lib; {
-  options = {
-    bundles.nodejs.enable = mkModuleEnableOption {
-      name = "Node.js Tools Bundle";
-      description = "Development tools for the Node.js JavaScript runtime";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    interpreters.nodejs.enable = mkDefault true;
-    languageServers.typescript-language-server.enable = mkDefault true;
-    debuggers.node-dap.enable = mkDefault true;
-    formatters.prettier.enable = mkDefault true;
-    packagers.nodejs-packager.enable = mkDefault true;
-  };
+{ pkgs, config, ... }:
+pkgs.lib.mkBundleModule {
+  id = "nodejs";
+  name = "Node.js Tools Bundle";
+  description = "Development tools for the Node.js JavaScript runtime";
+  submodules = [
+    "interpreters.nodejs"
+    "languageServers.typescript-language-server"
+    "debuggers.node-dap"
+    "formatters.prettier"
+    "packagers.nodejs-packager"
+  ];
+  inherit pkgs config;
 }
--- pkgs/modules/bundles/web/default.nix
+++ pkgs/modules/bundles/web/default.nix
@@ -1,18 +1,12 @@
-{ pkgs, lib, config, ... }:
-let cfg = config.bundles.web;
-in with pkgs.lib; {
-  options = {
-    bundles.web.enable = mkModuleEnableOption {
-      name = "Web";
-      description = "Tools for web development";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    languageServers.typescript-language-server.enable = mkDefault true;
-    languageServers.typescript-language-server.extensions = mkDefault [ ".js" ".jsx" ".ts" ".tsx" ".mjs" ".mts" ".cjs" ".cts" ".es6" ];
-
-    languageServers.html-language-server.enable = mkDefault true;
-    languageServers.css-language-server.enable = mkDefault true;
-  };
+{ pkgs, config, ... }:
+pkgs.lib.mkBundleModule {
+  id = "web";
+  name = "Web Tools Bundle";
+  description = "Tools for web development";
+  submodules = [
+    "languageServers.typescript-language-server"
+    "languageServers.html-language-server"
+    "languageServers.css-language-server"
+  ];
+  inherit pkgs config;
 }
