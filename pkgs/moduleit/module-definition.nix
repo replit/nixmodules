16987e10b83511b0a74691d8c5e9aeab5deccad2
Toby Ho <toby@repl.it>
2024-06-05 15:30:41 -0400

    typo

--- pkgs/moduleit/module-definition.nix
+++ pkgs/moduleit/module-definition.nix
@@ -634,7 +634,7 @@ in
       type = types.bool;
       default = false;
       description = lib.mdDoc ''
-        Whether this module demoted.
+        Whether this module is demoted.
       '';
     };
 
