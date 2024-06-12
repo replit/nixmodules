0aa5bc13addc2ab74121100af967c49b7c44857d
7heMech <83923848+7heMech@users.noreply.github.com>
2024-06-12 05:53:57 +0000

    bun: update to 1.1.13
    This update has some WebSocket memory leak fixes which I need.

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.1.11";
+  version = "1.1.13";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-RfP4fnTqumZKMBvC3ze4Lb7cQuUVl/czL+8xqB00kPA=";
+    hash = "sha256-QC6dsWjRYiuBIojxPvs8NFMSU6ZbXbZ9Q/+u+45NmPc=";
   };
 }
