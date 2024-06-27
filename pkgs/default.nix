b6398a24eaf1d4a40ec2f04cb22b29fc01b0bc11
Toby Ho <toby@repl.it>
2024-06-27 17:52:31 +0000

    revert dev module ids

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,7 +7,7 @@ let
   revstring_long = self.rev or "dirty";
   revstring = builtins.substring 0 7 revstring_long;
 
-  dev-module-ids = [ "python-3.10" "nodejs-18" "nodejs-20" "nodejs-with-prybar-18" "docker" "python-with-prybar-3.10" ];
+  dev-module-ids = [ "python-3.10" "nodejs-18" "nodejs-20" "docker" "replit" "replit-rtld-loader" ];
 
   mkPhonyOCI = pkgs.callPackage ./mk-phony-oci { ztoc-rs = self.inputs.ztoc-rs.packages.x86_64-linux.default; };
 
