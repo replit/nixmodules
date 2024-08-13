883a9343ca7ac1e486a7c93d8cf664fb6ff8e3d7
Toby Ho <toby@repl.it>
2024-08-13 17:02:22 -0400

    updated poetry

--- pkgs/poetry/poetry-py3.8.nix
+++ pkgs/poetry/poetry-py3.8.nix
@@ -1,7 +1,7 @@
 { pkgs, python, pypkgs }:
 pkgs.callPackage ./poetry-in-venv.nix {
-  version = "1.5.4";
-  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.4-python-3.8.17-bundle.tgz;
+  version = "1.5.5";
+  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.5-python-3.8.18-bundle.tgz;
   sha256 = "sha256:061zcr31caf5p5qi7jxqz7hnw5ic26737lykd8vjs4yl4w9qsrwd";
   inherit python pypkgs;
 }
