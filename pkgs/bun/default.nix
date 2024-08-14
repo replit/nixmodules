a02e5b9bcc1861bfb2ebc939c064da1be756e014
7heMech <83923848+7heMech@users.noreply.github.com>
2024-08-14 14:16:45 +0000

    Fix regression - .24

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.1.23";
+  version = "1.1.24";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-6b+X+ynbkECEOFssPUhiEiKOQDZ4xydZp5Ewd5kFAhY=";
+    hash = "sha256-9HiIBnjPKC/dpGzJ1Y4goN3h+tjJz+4Sjq1DmWRT4cg=";
   };
 }
