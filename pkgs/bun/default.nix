18865c1c26588badcea58397d367b7b544229585
7heMech <83923848+7heMech@users.noreply.github.com>
2024-08-14 08:17:51 +0000

    4mb smaller, 50% faster console.log, 30% faster fetch decompression...

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.1.21";
+  version = "1.1.23";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-KPq1T0jxN6ghFIoKRe3rQqUtI6qMuRdMzIFVSj+JMco=";
+    hash = "sha256-6b+X+ynbkECEOFssPUhiEiKOQDZ4xydZp5Ewd5kFAhY=";
   };
 }
