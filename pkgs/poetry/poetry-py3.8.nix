f3e1935d35215b4683eaa134e19cbaa118192b57
Devon Stewart <blast@hardchee.se>
2024-09-20 20:52:07 +0000

    Moving py3.8 over to historical modules

--- pkgs/poetry/poetry-py3.8.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, python, pypkgs }:
-pkgs.callPackage ./poetry-in-venv.nix {
-  version = "1.5.5";
-  url = https://storage.googleapis.com/poetry-bundles/poetry-1.5.5-python-3.8.18-bundle.tgz;
-  sha256 = "sha256:0iaw6iyj5skbi2vs5sfcbwlncxjfq3i2k1nnfvrdc02ybbmpbl3q";
-  inherit python pypkgs;
-}
