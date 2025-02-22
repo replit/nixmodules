ea9c9a3104313cd86bfe187bc758cfda61022f64
7heMech <83923848+7heMech@users.noreply.github.com>
2025-02-22 08:50:18 +0000

    Bun 1.2.3

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.2.2";
+  version = "1.2.3";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-P077iv0fhKwqmMBGYciYVh0dNVJ9Awy0Vx6Zt8hfUHk=";
+    hash = "sha256-wNNfzF/rCOi4SDLr5hcS0r+x8Rgd5wbpOHvBLiNGPzU=";
   };
 }
