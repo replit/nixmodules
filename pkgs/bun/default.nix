eef1f3e6b17d221bb7ee24f415b51e09295f0828
Severin Ibarluzea <seve700@gmail.com>
2025-05-09 19:02:22 -0700

    Update to bun 1.2.12

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.2.5";
+  version = "1.2.12";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-iPZL7e4zD/TWMo4+kMZpvHrFMUknxgT4XjKeoqHRl5s=";
+    hash = "sha256-MzeWT6ox2LzhmfkXCBMHEO8ucLmP80o09ZCxdFiApjM";
   };
 }
