2936b1cfef3d15b1054dc8d3c0f7ed5ca0676192
7heMech <83923848+7heMech@users.noreply.github.com>
2025-09-02 13:48:07 +0000

    idk atp

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.2.21";
+  version = "1.2.18";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha-124356";
+    hash = "sha256-n/HcFSSNtnmzEa4nBuzn8LuJ/IQ3/QUlTt3OO5T/ACQ=";
   };
 }
