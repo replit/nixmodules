748ad3c7099b872162f89619ce879c11573d4f16
7heMech's Bun Updater <83923848+7heMech@users.noreply.github.com>
2025-09-02 13:50:21 +0000

    Update Bun to version 1.2.21!

--- pkgs/bun/default.nix
+++ pkgs/bun/default.nix
@@ -3,9 +3,9 @@
 }:
 
 bun.overrideAttrs rec {
-  version = "1.2.18";
+  version = "1.2.21";
   src = fetchurl {
     url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-    hash = "sha256-n/HcFSSNtnmzEa4nBuzn8LuJ/IQ3/QUlTt3OO5T/ACQ=";
+    hash = "sha256-WU9FTVHOVxmdQyDIXL1JW+nAVO8XquvKXmyQir/aYXk=";
   };
 }
